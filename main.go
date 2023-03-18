52841db110e45e810ad59003513e8316917da749
tokizuoh <tokizuoh@gmail.com>
2023-03-18 19:43:55 +0900

    refactor main method into smaller functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -9,6 +10,56 @@ import (
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 )
+
+type Context struct {
+	CheckRun struct {
+		Conclusion githubv4.CheckConclusionState
+		DetailsUrl githubv4.URI
+		Name       string
+	} `graphql:"... on CheckRun"`
+	StatusContext struct {
+		State     githubv4.StatusState
+		TargetUrl githubv4.URI
+		Context   string
+	} `graphql:"... on StatusContext"`
+}
+
+type Query struct {
+	Repository struct {
+		Object struct {
+			Commit struct {
+				StatusCheckRollup struct {
+					Contexts struct {
+						Nodes []Context
+					} `graphql:"contexts(first: 100)"`
+				}
+				AbbreviatedOid string
+			} `graphql:"... on Commit"`
+		} `graphql:"object(expression: $expression)"`
+	} `graphql:"repository(owner: $owner, name: $name)"`
+}
+
+func checkAllSuccess(ctxs []Context) (bool, error) {
+	f := true
+	for _, ctx := range ctxs {
+		if ctx.CheckRun.Name != "" {
+			if ctx.CheckRun.Name == "auto-merge-helper" {
+				continue
+			}
+			if ctx.CheckRun.Conclusion != githubv4.CheckConclusionStateSuccess {
+				f = false
+			}
+		} else if ctx.StatusContext.Context != "" {
+			if ctx.StatusContext.State != githubv4.StatusStateSuccess {
+				f = false
+			}
+		} else {
+			return false, fmt.Errorf("invalid inline fragment: does not match the expected type")
+		}
+	}
+
+	return f, nil
+}
 
 func main() {
 	token := os.Getenv("GITHUB_TOKEN")
@@ -31,34 +82,7 @@ func main() {
 	httpClient := oauth2.NewClient(context.Background(), src)
 	client := githubv4.NewClient(httpClient)
 
-	type Context struct {
-		CheckRun struct {
-			Conclusion githubv4.CheckConclusionState
-			DetailsUrl githubv4.URI
-			Name       string
-		} `graphql:"... on CheckRun"`
-		StatusContext struct {
-			State     githubv4.StatusState
-			TargetUrl githubv4.URI
-			Context   string
-		} `graphql:"... on StatusContext"`
-	}
-
-	var query struct {
-		Repository struct {
-			Object struct {
-				Commit struct {
-					StatusCheckRollup struct {
-						Contexts struct {
-							Nodes []Context
-						} `graphql:"contexts(first: 100)"`
-					}
-					AbbreviatedOid string
-				} `graphql:"... on Commit"`
-			} `graphql:"object(expression: $expression)"`
-		} `graphql:"repository(owner: $owner, name: $name)"`
-	}
-
+	query := Query{}
 	variables := map[string]interface{}{
 		"owner":      githubv4.String(owner),
 		"name":       githubv4.String(repositoryName),
@@ -70,25 +94,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	f := true
-	for _, node := range query.Repository.Object.Commit.StatusCheckRollup.Contexts.Nodes {
-		if node.CheckRun.Name != "" {
-			if node.CheckRun.Name == "auto-merge-helper" {
-				continue
-			}
-			if node.CheckRun.Conclusion != githubv4.CheckConclusionStateSuccess {
-				f = false
-			}
-		} else if node.StatusContext.Context != "" {
-			if node.StatusContext.State != githubv4.StatusStateSuccess {
-				f = false
-			}
-		} else {
-			log.Fatal("invalid inline fragment: does not match the expected type")
-		}
+	ok, err := checkAllSuccess(query.Repository.Object.Commit.StatusCheckRollup.Contexts.Nodes)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	if f {
+	if ok {
 		log.Println("all success")
 	} else {
 		log.Fatalln("failed to run CI successfully, at least one CI has failed")
