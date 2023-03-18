eb1434f9c76f25d9cccb9e11042299a610c1cb7e
tokizuoh <tokizuoh@gmail.com>
2023-03-18 13:16:38 +0900

    add processing to query using shurcooL/githubv4

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/tokizuoh/auto-merge-helper
 
 go 1.20
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/shurcooL/githubv4 v0.0.0-20230305132112-efb623903184 // indirect
+	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,61 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"context"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	body := []byte(``)
-	buf := bytes.NewBuffer(body)
-	req, err := http.NewRequest("POST", "https://api.github.com/graphql", buf)
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
+
+	type _context struct {
+		CheckRun struct {
+			Conclusion githubv4.CheckConclusionState
+			DetailsUrl githubv4.URI
+			Name       string
+		} `graphql:"... on CheckRun"`
+		StatusContext struct {
+			State     githubv4.StatusState
+			TargetUrl githubv4.URI
+			Context   string
+		} `graphql:"... on StatusContext"`
+	}
+
+	var query struct {
+		Repository struct {
+			Object struct {
+				Commit struct {
+					StatusCheckRollup struct {
+						Contexts struct {
+							Nodes []_context
+						} `graphql:"contexts(first: 100)"`
+					}
+					AbbreviatedOid string
+				} `graphql:"... on Commit"`
+			} `graphql:"object(expression: \"8cab9ceabc5e0af9a6d407b80357a\")"`
+		} `graphql:"repository(owner: \"tokizuoh\", name: \"citrus\")"`
+	}
+	err := client.Query(context.Background(), &query, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		log.Fatal("no such environment variable: GITHUB_TOKEN")
+	// TODO: Handle
+	for _, n := range query.Repository.Object.Commit.StatusCheckRollup.Contexts.Nodes {
+		// log.Println(n.CheckRun.Name)
+		// log.Println(n.CheckRun.Conclusion)
+		log.Println(n.CheckRun.DetailsUrl)
+		// log.Println(n.StatusContext.Context)
+		// log.Println(n.StatusContext.State)
+		log.Println(n.StatusContext.TargetUrl)
+		log.Println("---")
 	}
-
-	req.Header.Add("Authorization", fmt.Sprintf("bearer %s", token))
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// TODO
-	log.Println(res)
 }
