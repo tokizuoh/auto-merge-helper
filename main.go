bb4b9afb57fcc292bf883d5a83a9326332556685
tokizuoh <tokizuoh@gmail.com>
2023-03-18 12:02:03 +0900

    add empty processing http request

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	body := []byte(``)
+	buf := bytes.NewBuffer(body)
+	req, err := http.NewRequest("POST", "https://api.github.com/graphql", buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("no such environment variable: GITHUB_TOKEN")
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("bearer %s", token))
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO
+	log.Println(res)
 }
